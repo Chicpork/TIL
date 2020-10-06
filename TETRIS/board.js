6879b6f9354889288bf1bee4b7d73b372b864b1e
Chicpork <dkgkrmfo@gmail.com>
2020-10-07 00:01:29 +0900

    make tetris

--- TETRIS/board.js
+++ TETRIS/board.js
@@ -10,4 +10,22 @@ class Board {
       { length: ROWS }, () => Array(COLS).fill(0)
     );
   }
+
+  valid(p) {
+    return p.shape.every((row, dy) => {
+      return row.every((value, dx) => {
+        let x = p.x + dx;
+        let y = p.y + dy;
+        return value === 0 || (this.isInsideWalls(x, y) && this.notOccupied(x, y));
+      });
+    });
+  }
+
+  isInsideWalls(x, y) {
+    return x >= 0 && x < COLS && y <= ROWS;
+  }
+
+  notOccupied(x, y) {
+    return this.grid[y] && this.grid[y][x] === 0;
+  }
 }
