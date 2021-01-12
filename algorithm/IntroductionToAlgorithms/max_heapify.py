f78bcae094f9cb3f874486da345c65f933608559
Chicpork <dkgkrmfo@gmail.com>
2021-01-12 21:30:41 +0900

    Update min_heapify

--- algorithm/IntroductionToAlgorithms/max_heapify.py
+++ algorithm/IntroductionToAlgorithms/max_heapify.py
@@ -4,30 +4,28 @@ def max_heapify(A, i):
     max_index = i
     left = 2*i
     right = left+1
-    if left <= len(A) and A[max_index-1] <= A[left-1]:
+    if left <= len(A) and A[max_index-1] < A[left-1]:
         max_index = left
     
-    if right <= len(A) and A[max_index-1] <= A[right-1]:
+    if right <= len(A) and A[max_index-1] < A[right-1]:
         max_index = right
     
     if max_index != i:
         A[i-1], A[max_index-1] = A[max_index-1], A[i-1]
-        print(max_index, A)
         max_heapify(A, max_index)
 
 def max_heapify2(A, i):
     max_index = i
     left = 2*(i+1) - 1
     right = left+1
-    if left <= len(A) and A[max_index] <= A[left]:
+    if left <= len(A) and A[max_index] < A[left]:
         max_index = left
     
-    if right <= len(A) and A[max_index] <= A[right]:
+    if right <= len(A) and A[max_index] < A[right]:
         max_index = right
     
     if max_index != i:
         A[i], A[max_index] = A[max_index], A[i]
-        print(max_index, A)
         max_heapify2(A, max_index)
 
 def max_heapify3(A, i):
@@ -35,20 +33,37 @@ def max_heapify3(A, i):
     max_index = index_i
     left = 2*i - 1
     right = left + 1
-    if left < len(A) and A[max_index] <= A[left]:
+    if left < len(A) and A[max_index] < A[left]:
         max_index = left
     
-    if right < len(A) and A[max_index] <= A[right]:
+    if right < len(A) and A[max_index] < A[right]:
         max_index = right
     
     if max_index != index_i:
         A[index_i], A[max_index] = A[max_index], A[index_i]
-        print(max_index, A)
         max_heapify3(A, max_index + 1)
+
+def min_heapify(A, i):
+    min_index = i
+    left = 2*i
+    right = left+1
+    if left <= len(A) and A[min_index-1] > A[left-1]:
+        min_index = left
     
-# max_heapify(A,3)
+    if right <= len(A) and A[min_index-1] > A[right-1]:
+        min_index = right
+    
+    if min_index != i:
+        A[i-1], A[min_index-1] = A[min_index-1], A[i-1]
+        min_heapify(A, min_index)
+
+
+# max_heapify(A,3) # [27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0]
 # max_heapify2(A,2)
-max_heapify3(A,3)
+# max_heapify3(A,3)
+
+
+min_heapify(A, 3) # [27, 17, 1, 16, 13, 10, 3, 5, 7, 12, 4, 8, 9, 0]
+
 print(A)
 
-# [27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0]
