44f1cf56759c3866c04bbf0e2f708a305591ce7b
Chicpork <dkgkrmfo@gmail.com>
2021-03-07 23:15:42 +0900

    Update logging

--- apt/sample.py
+++ apt/sample.py
@@ -15,10 +15,24 @@ with open("./apt/logging.json", "r") as f:
     config = json.load(f)
 
 logging.config.dictConfig(config)
+
 logger = logging.getLogger(__name__)
 
-
 def get_apt_trsc_data(deal_ymd, lawd_cd, num_of_rows, page_no):
+    """
+    Args:
+        deal_ymd (str): 매매거래년월
+        lawd_cd (str): 지역구분코드
+        num_of_rows (int): 1회 조회 건수
+        page_no (int): page 번호
+
+    Returns:
+        (result_code, data, totalCount)
+
+    Note:
+        result_code: 1=정상, 0=조회가능회수 초과, -1=에러
+
+    """
     url = 'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev'
     queryParams = '?' + 'ServiceKey=' + myConfig.DATAGO_CONFIG['ServiceKey'] + '&'\
                       + parse.urlencode({ parse.quote_plus('pageNo') : page_no
@@ -30,33 +44,34 @@ def get_apt_trsc_data(deal_ymd, lawd_cd, num_of_rows, page_no):
         response = requests.get(url + queryParams)
     except requests.exceptions.RequestException:
         logger.error("Cannot get result " + ",".join([deal_ymd, lawd_cd, str(num_of_rows), str(page_no)]))
-        return (False, '', -1)
+        return (-1, '', -1)
     else:
         if response.status_code == requests.codes.ok:
             tree = elemTree.fromstring(response.text)
             if tree.find('./header/resultCode').text == '00': # normal response
-                return (True, response.text, tree.find('./body/totalCount').text)
+                return (1, response.text, tree.find('./body/totalCount').text)
+            elif tree.find('./header/resultCode').text == '99': # LIMITED NUMBER OF SERVICE REQUESTS EXCEEDS ERROR.
+                logger.error("Get LIMITED NUMBER Exceed " + ",".join([deal_ymd, lawd_cd, str(num_of_rows), str(page_no), response.text]))
+                return (0, '', -1)
             else:
                 logger.error("Cannot get result " + ",".join([deal_ymd, lawd_cd, str(num_of_rows), str(page_no), response.text]))
-                return (False, '', -1)
+                return (-1, '', -1)
         
-    return (False, '', -1)
+    return (-1, '', -1)
 
 def get_result_and_save(deal_ymd, lawd_cd, num_of_rows, page_no, path):
-    result = get_apt_trsc_data(deal_ymd, lawd_cd, num_of_rows, page_no)
+    result_code, data, totalCount = get_apt_trsc_data(deal_ymd, lawd_cd, num_of_rows, page_no)
     
-    if not result[0]:
-        result = get_apt_trsc_data(deal_ymd, lawd_cd, num_of_rows, page_no)
-        if not result[0]:
-            logger.error("Cannot get result " + ",".join([deal_ymd, lawd_cd, str(num_of_rows), str(page_no)]))
+    if result_code == -1:
+        result_code, data, totalCount = get_apt_trsc_data(deal_ymd, lawd_cd, num_of_rows, page_no)
     
-    if result[0]:
-        file_name =  "/"+ "_".join([lawd_cd, result[2], str(num_of_rows), str(page_no)]) + ".xml"
+    if result_code == 1:
+        file_name =  "/"+ "_".join([lawd_cd, totalCount, str(num_of_rows), str(page_no)]) + ".xml"
         with open(path+file_name, 'w', encoding='utf8') as f:
-            f.write(result[1])
-        return int(result[2])
+            f.write(data)
+        return (result_code, int(totalCount))
     
-    return -1
+    return (result_code, -1)
 
 def test2():
     with open('./apt/regCode_ncrg.txt', encoding='utf8') as f:
@@ -80,7 +95,9 @@ def test2():
                 logger.info("Saved Data Not Exists.. " + ",".join([deal_ymd, regcode_ncrg]))
                 cur_cnt = 1
 
-                tot_cnt = get_result_and_save(deal_ymd, regcode_ncrg, num_of_rows, cur_cnt, deal_ymd_path.as_posix())
+                result, tot_cnt = get_result_and_save(deal_ymd, regcode_ncrg, num_of_rows, cur_cnt, deal_ymd_path.as_posix())
+                if result == 0:
+                    return
                 cur_cnt += 1
                 
                 while True:                    
@@ -88,7 +105,9 @@ def test2():
                     if (cur_cnt-1)*num_of_rows >= tot_cnt:
                         break
                     
-                    get_result_and_save(deal_ymd, regcode_ncrg, num_of_rows, cur_cnt, deal_ymd_path.as_posix())
+                    result, tot_cnt = get_result_and_save(deal_ymd, regcode_ncrg, num_of_rows, cur_cnt, deal_ymd_path.as_posix())
+                    if result == 0:
+                        return
                     cur_cnt += 1
             else:
                 logger.info("Data Exists.. " + deal_ymd + "," + regcode_ncrg)
@@ -102,8 +121,11 @@ def test2():
                     file_name = "_".join([file_infos[0], file_infos[1], file_infos[2], str(page_no)]) + ".xml"
                     if not deal_ymd_path.joinpath(file_name).exists():
                         logger.info("Data Exists Get.. " + ",".join([deal_ymd, regcode_ncrg, str(page_no)]))
-                        get_result_and_save(deal_ymd, regcode_ncrg, num_of_rows, page_no, deal_ymd_path.as_posix())
+                        result, tot_cnt = get_result_and_save(deal_ymd, regcode_ncrg, num_of_rows, page_no, deal_ymd_path.as_posix())
+                        if result == 0:
+                            return
 
 
 if __name__ == "__main__":
     test2()
+    pass
