c122efc77fd6e79215d5f1f78dfbe81039c56643
Chicpork <dkgkrmfo@gmail.com>
2021-03-13 08:53:15 +0900

    Make aptCollector Using ray

--- apt/aptCollector_multiprocessing.py
+++ apt/aptCollector_multiprocessing.py
@@ -6,19 +6,13 @@ import json
 import logging
 import logging.config
 from pathlib import Path
-import os
 import xml.etree.ElementTree as elemTree
 import math
-import ray
-import psutil
-from ray.util import queue
+import multiprocessing
 
 # make logger
 with open("./apt/logging.json", "r") as f:
     config = json.load(f)
-
-num_cpus = 10
-ray.init(num_cpus=num_cpus)
 
 class SingletonInstance:
     __instance = None
@@ -33,7 +27,6 @@ class SingletonInstance:
         cls.instance = cls.__getInstance
         return cls.__instance
 
-@ray.remote
 class aptCollector(SingletonInstance):
     service_keys = None
 
@@ -73,7 +66,7 @@ class aptCollector(SingletonInstance):
 
         """
         url = 'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev'
-        queryParams = '?' + 'ServiceKey=' + aptCollector.service_keys[0] + '&'\
+        queryParams = '?' + 'ServiceKey=' + aptCollector.service_keys[0] + '&' \
                         + parse.urlencode({ parse.quote_plus('pageNo') : self.page_no
                                             , parse.quote_plus('numOfRows') : self.num_of_rows
                                             , parse.quote_plus('LAWD_CD') : self.lawd_cd
@@ -102,7 +95,7 @@ class aptCollector(SingletonInstance):
             if response.status_code == requests.codes.ok:
                 tree = elemTree.fromstring(response.text)
                 if tree.find('./header/resultCode').text == '00': # normal response
-                    self.logger.info("Get success {},{},{},{},{}".format(self.deal_ymd, self.lawd_cd, str(self.num_of_rows), str(self.page_no), response.text[-50:]))
+                    self.logger.info("Get success {},{},{},{},{}".format(self.deal_ymd, self.lawd_cd, str(self.num_of_rows), str(self.page_no), response.text[0:50]))
                     return (1, response.text, tree.find('./body/totalCount').text)
                 elif tree.find('./header/resultCode').text == '99': # LIMITED NUMBER OF SERVICE REQUESTS EXCEEDS ERROR.
                     self.logger.error("Get LIMITED NUMBER Exceed {},{},{},{},{}".format(self.deal_ymd, self.lawd_cd, str(self.num_of_rows), str(self.page_no), response.text))
@@ -119,13 +112,15 @@ class aptCollector(SingletonInstance):
             f.write(data)
     
     def __set_variables(self, data):
+        if aptCollector.service_keys is None:
+            aptCollector.service_keys = myConfig.DATAGO_CONFIG['ServiceKeys']
         self.path = data[0]
         self.deal_ymd = data[1]
         self.lawd_cd = data[2]
         self.num_of_rows = data[3]
         self.total_cnt = data[4]
         self.page_no = data[5]
-
+    
     def run(self, queue_datas):
         while not queue_datas.empty():
             self.__set_variables(queue_datas.get())
@@ -154,7 +149,7 @@ class aptCollector(SingletonInstance):
                 else:
                     raise ValueError("Invalid result_code [" + result_code + "]")
 
-def get_jobLists(ray_queue):
+def get_joblists(queue):
     with open('./apt/regCode_ncrg.txt', encoding='utf8') as f:
         lawd_cds = f.read().splitlines()
 
@@ -173,7 +168,7 @@ def get_jobLists(ray_queue):
             lawd_cd_files = [file.name for file in deal_ymd_path.glob(lawd_cd+"_" + str(num_of_rows) +"_*.xml")]
             
             if len(lawd_cd_files) == 0:
-                ray_queue.put((deal_ymd_path.as_posix(), deal_ymd, lawd_cd, num_of_rows, None, 1))
+                queue.put((deal_ymd_path.as_posix(), deal_ymd, lawd_cd, num_of_rows, None, 1))
             else:
                 # file_infos[0]: region cd
                 # file_infos[1]: num rows per 1 request
@@ -184,12 +179,19 @@ def get_jobLists(ray_queue):
                 for page_no in range(1, math.ceil(int(file_infos[2])/int(file_infos[1]))+1):
                     file_name = "{}_{}_{}_{}.xml".format(file_infos[0], file_infos[1], file_infos[2], str(page_no))
                     if not deal_ymd_path.joinpath(file_name).exists():
-                        ray_queue.put((deal_ymd_path.as_posix(), deal_ymd, lawd_cd, num_of_rows, file_infos[2], page_no))
+                        queue.put((deal_ymd_path.as_posix(), deal_ymd, lawd_cd, num_of_rows, file_infos[2], page_no))
 
 
 if __name__ == "__main__":
-    aptCollector_actors = [aptCollector.remote() for _ in range(num_cpus)]
-    ray_queue = queue.Queue()
-    ray.put(get_jobLists(ray_queue))
-
-    results = ray.get([aptCollector_actor.run.remote(ray_queue) for aptCollector_actor in aptCollector_actors])
+    shared_queue = multiprocessing.Manager().Queue()
+    get_joblists(shared_queue)
+    
+    process_num = 10
+    t_processs = []
+    for ix in range(process_num):
+        t_process = multiprocessing.Process(target=aptCollector().run, args=(shared_queue, ), name="process_"+str(ix), daemon=True)
+        t_process.start()
+        t_processs.append(t_process)
+        
+    for t_process in t_processs:
+        t_process.join()
