fbc4c926200a3cba2ce69b80f2d0477f5e8bf01a
Chicpork <41892528+Chicpork@users.noreply.github.com>
2026-04-23 15:18:31 +0900

    Delete fokin-weather directory

--- fokin-weather/components/CustomSwitch.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Switch } from "react-native-paper";
-import { PreferencesContext } from "../contexts/PreferencesContext";
-
-export default function CustomSwitch() {
-  const { toggleTheme, isThemeDark } = React.useContext(PreferencesContext);
-
-  return <Switch value={isThemeDark} onValueChange={toggleTheme} />;
-}
--- fokin-weather/screens/DisclosureScreen.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-
-export default function DisclosureScreen() {
-  return <Text>공시</Text>;
-}
